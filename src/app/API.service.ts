0dbc2dd7a7f26ea2a1915a0e900b1c2a2c1cf67b
Zhengyang Wu <Zhengyang.Wu@masterelectronics.com>
2021-05-25 11:42:01 -0700

    upload test demo

--- src/app/API.service.ts
+++ src/app/API.service.ts
@@ -1,23 +1,23 @@
 /* tslint:disable */
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
-import { Injectable } from '@angular/core';
-import API, { graphqlOperation, GraphQLResult } from '@aws-amplify/api-graphql';
-import { Observable } from 'zen-observable-ts';
+import { Injectable } from "@angular/core";
+import API, { graphqlOperation, GraphQLResult } from "@aws-amplify/api-graphql";
+import { Observable } from "zen-observable-ts";
 
 export type InputCreateCar = {
   color: string;
 };
 
 export type Car = {
-  __typename: 'Car';
+  __typename: "Car";
   id?: string;
   color?: string;
   parking?: Parking;
 };
 
 export type Parking = {
-  __typename: 'Parking';
+  __typename: "Parking";
   id?: string;
   name?: string;
   car?: Car;
@@ -25,15 +25,15 @@ export type Parking = {
 };
 
 export type CreateCarMutation = {
-  __typename: 'Car';
+  __typename: "Car";
   id: string;
   color: string;
   parking?: {
-    __typename: 'Parking';
+    __typename: "Parking";
     id: string;
     name: string;
     car: {
-      __typename: 'Car';
+      __typename: "Car";
       id: string;
       color: string;
     };
@@ -42,15 +42,15 @@ export type CreateCarMutation = {
 };
 
 export type GetParkingQuery = {
-  __typename: 'Parking';
+  __typename: "Parking";
   id: string;
   name: string;
   car: {
-    __typename: 'Car';
+    __typename: "Car";
     id: string;
     color: string;
     parking?: {
-      __typename: 'Parking';
+      __typename: "Parking";
       id: string;
       name: string;
       price: number;
@@ -60,18 +60,21 @@ export type GetParkingQuery = {
 };
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class APIService {
   async CreateCar(input?: InputCreateCar): Promise<CreateCarMutation> {
     const statement = `mutation CreateCar($input: InputCreateCar) {
         createCar(input: $input) {
+          __typename
           id
           color
           parking {
+            __typename
             id
             name
             car {
+              __typename
               id
               color
             }
@@ -91,12 +94,15 @@ export class APIService {
   async GetParking(id: string): Promise<GetParkingQuery> {
     const statement = `query GetParking($id: ID!) {
         getParking(id: $id) {
+          __typename
           id
           name
           car {
+            __typename
             id
             color
             parking {
+              __typename
               id
               name
               price
@@ -106,7 +112,7 @@ export class APIService {
         }
       }`;
     const gqlAPIServiceArguments: any = {
-      id,
+      id
     };
     const response = (await API.graphql(
       graphqlOperation(statement, gqlAPIServiceArguments)
