f9f526ef06f7db63eb3a0e2b65ca3c00a270c0f5
zhengyang.wu <zhengyang.wu@masterelectronics.com>
2022-05-02 15:52:31 -0700

    test scirpt for login

--- cypress/integration/Login/login_spec.js
+++ cypress/integration/Login/login_spec.js
@@ -1,6 +1,6 @@
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:4200/');
+    cy.visit('/');
   });
 
   it('login wrong password', () => {
@@ -14,6 +14,8 @@ describe('Login page', () => {
     cy.get('form').find('input[id="username"]').type('salive');
     cy.get('form').find('input[id="password"]').type('Master@2425');
     cy.get('form').find('button[id="login"]').click();
+    // should be redirected to /dashboard
     cy.url().should('include', '/home');
+    // auth session should be present
   });
 });
