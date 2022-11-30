ff8892511cc685fd166405a1e9ce5978ee979b13
zhengyang.wu <zhengyang.wu@masterelectronics.com>
2022-11-30 13:30:08 -0700

    change the dev enviment's url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://10.10.1.88:4000',
-  graphql: 'http://10.10.1.88:3000/graphql',
+  apiUrl: 'http://meow-aws-wdxd2:4000',
+  graphql: 'http://meow-aws-wdxd2:3000/graphql',
   GTM_ID: 'GTM-KS8744D',
   changelogurl: 'https://master-electronics.gitbook.io/wms/',
   DistributionCenter: 'PH',
