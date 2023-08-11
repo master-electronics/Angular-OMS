0adc3dc9303d330715ffc6db3c8d76b7cf0b00dd
zhengyang.wu <zhengyang.wu@masterelectronics.com>
2023-08-11 14:59:28 -0700

    init inventory menegement module

---
+++ src/app/pages/inventory-management/feature/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <div class="container mx-auto px-2 py-2 text-lg md:mt-4">
+      <div class="mt-5 grid justify-center gap-20 text-base">
+        <a class="" routerLink="/inventorymanagement/audit">Audit</a>
+        <a class="" routerLink="/inventorymanagement/systemtrigger"
+          >System Trigger</a
+        >
+        <a class="" routerLink="/inventorymanagement/usertrigger"
+          >User Trigger</a
+        >
+        <a class="" routerLink="/inventorymanagement/management">Management</a>
+      </div>
+    </div>
+  `,
+})
+export class MenuComponent {}
