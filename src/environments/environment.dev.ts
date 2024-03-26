9c8b0a01db6a0167e667621b738d9f23e97c5c26
Zhengyang Wu <zhengyang.wu@masterelectronics.com>
2024-03-26 13:21:58 -0700

    add enviroment variable for new nestjs server

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: false,
   apiUrl: 'http://10.10.1.88:4000',
-  omsUrl: 'http://10.10.1.88:5051',
+  authUrl: 'http://10.10.1.88:6001',
   graphql: 'http://10.10.1.88:3000/graphql',
+  nestjsGateway: 'http://10.10.1.88:6500/graphql',
   GTM_ID: 'GTM-KS8744D',
   changelogurl: 'https://master-electronics.gitbook.io/wms/',
   idleTimeInMinutes: 20,
